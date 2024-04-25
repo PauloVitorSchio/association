50c7b3015f68c331b67c1ae2a9350a16430a7cbd
Paulo Vitor Schio <paulovschio@gmail.com>
2024-04-25 17:39:59 -0300

    Utilizando métodos privados na classe Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @dashboard = Dashboard.new(current_user)
+    @dashboard = Dashboard.new(current_user).charge
   end
 end
--- app/services/dashboard.rb
+++ app/services/dashboard.rb
@@ -1,48 +1,71 @@
 class Dashboard
+
+  attr_reader :active_people_pie_chart, :total_debts, :total_payments, :balance, :last_debts, :last_payments,
+  :my_people, :top_person, :bottom_person
+
   def initialize(current_user)
     @current_user = current_user
   end
 
-  def active_people_pie_chart
-    return {
+  def charge
+    populate_data
+  end
+
+  private
+
+  def populate_data
+    set_active_people_pie_chart
+    set_total_debts
+    set_total_payments
+    set_balance
+    set_last_debts
+    set_last_payments
+    set_my_people
+    set_top_person
+    set_bottom_person
+    self
+  end
+
+  def set_active_people_pie_chart
+    @active_people_pie_chart = {
       active: Person.where(active: true).count,
       inactive: Person.where(active: false).count
     }
   end
 
-  def total_debts
-    return Debt.where(person: Person.where(active: true)).sum(:amount)
+  def set_total_debts
+    @total_debts = Debt.where(person: Person.where(active: true)).sum(:amount)
   end
 
-  def total_payments
-    return Payment.where(person: Person.where(active: true)).sum(:amount)
+  def set_total_payments
+    @total_payments = Payment.where(person: Person.where(active: true)).sum(:amount)
   end
 
-  def balance
-    return total_payments - total_debts
+  def set_balance
+    @balance = @total_payments - @total_debts
   end
 
-  def last_debts
-    return Debt.order(created_at: :desc).limit(10).map do |debt|
+  def set_last_debts
+    @last_debts = Debt.order(created_at: :desc).limit(10).map do |debt|
       [debt.id, debt.amount]
     end
   end
 
-  def last_payments
-    return Payment.order(created_at: :desc).limit(10).map do |payment|
+  def set_last_payments
+    @last_payments = Payment.order(created_at: :desc).limit(10).map do |payment|
       [payment.id, payment.amount]
     end
   end
 
-  def my_people
-    return Person.where(user: @current_user).order(:created_at).limit(10)
+  def set_my_people
+    @my_people = Person.where(user: @current_user).order(:created_at).limit(10)
   end
 
-  def top_person
-    return Person.where(active: true).order(balance: :desc).first
+  def set_top_person
+    @top_person = Person.where(active: true).order(balance: :desc).first
   end
 
-  def bottom_person
-    return Person.where(active: true).order(balance: :asc).first
+  def set_bottom_person
+    @bottom_person = Person.where(active: true).order(balance: :asc).first
   end
 end

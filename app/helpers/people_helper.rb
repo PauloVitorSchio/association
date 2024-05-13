1ba45a2f9df5de2f4fcef0201aae6ae55361b728
Paulo Vitor Schio <paulovschio@gmail.com>
2024-05-13 19:20:31 -0300

    Corrigir identação

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,9 +1,9 @@
 module PeopleHelper
     def active_class_for_person(person)
-    person.active? ? 'success' : 'danger'
+        person.active? ? 'success' : 'danger'
     end
 
     def icon_for_person(person)
-    person.active? ? 'check-circle' : 'x-circle'
+        person.active? ? 'check-circle' : 'x-circle'
     end
 end
